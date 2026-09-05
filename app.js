722b1588ac5f4f7bd0a836c366cf049d1a0d7a08
Phu-1966 <phupham2025@gmail.com>
2026-09-05 19:27:26 +0700

    Update ePub initialization with file name

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-const book = ePub(undefined, {});
-book.open ("phia-sau-buc-tuong.epub", "epub");
+const book = ePub("phia-sau-buc-tuong.epub", {});
 const rendition = book.renderTo("viewer", {
   width: "100%", height: "100%", spread: "none",
   minSpreadWidth: 9999,
