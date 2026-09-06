599ff387f08ec9fd704e00433f0473f84070424c
Phu-1966 <phupham2025@gmail.com>
2026-09-06 11:02:48 +0700

    Adjust padding in app.js from 6% to 5%

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ rendition.hooks.content.register(function(contents) {
         column-width: auto !important;
 
         -webkit-column-width: auto !important;
-        padding-left: 6% !important;
+        padding-left: 5% !important;
 
-padding-right: 6% !important;
+padding-right: 5% !important;
         
 overflow-x: hidden !important;
       }
