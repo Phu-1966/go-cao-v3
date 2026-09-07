f4541be1aa95dc8befdb88a22296965f1dd963fb
Phu-1966 <phupham2025@gmail.com>
2026-09-07 17:10:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,19 +14,13 @@ rendition.hooks.content.register(function(contents) {
     encodeURIComponent(`
 
       html, body {
-column-count: 1 !important;
 
-  -webkit-column-count: 1 !important;
-
-  column-width: auto !important;
-
-  -webkit-column-width: auto !important;
   
   padding-left: 2% !important;
 
   padding-right: 2% !important;
 
-  
+  display: inline-block !important;
 
 }
 
