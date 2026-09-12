2e5d84c42aa7f46c966d960993152a370f43331e
Phu-1966 <phupham2025@gmail.com>
2026-09-12 18:17:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,51 @@ gap: 0 ,
   manager: "default",
 
 });
+const manager = rendition.manager;
+
+const originalScrollBy = manager.scrollBy.bind(manager);
+
+manager.scrollBy = function (x, y, silent) {
+
+    originalScrollBy(x, y, silent);
+
+    if (!this.settings.fullsize) {
+
+        if (x) {
+
+            const remainder = this.container.scrollLeft % x;
+
+            if (Math.abs(x - remainder) >= 1) {
+
+                const compensationDir =
+
+                    Math.round(remainder / x) === 1 ? 1 : -1;
+
+                this.container.scrollLeft += compensationDir;
+
+            }
+
+        }
+
+        if (y) {
+
+            const remainder = this.container.scrollTop % y;
+
+            if (Math.abs(y - remainder) >= 1) {
+
+                const compensationDir =
+
+                    Math.round(remainder / y) === 1 ? 1 : -1;
+
+                this.container.scrollTop += compensationDir;
+
+            }
+
+        }
+
+    }
+
+};
 rendition.spread("none");
 
 let fontSize = 100;
@@ -90,15 +135,7 @@ async function turnPage(direction) {
 
     }
 
-    const manager = rendition.manager;
-
-    const view = manager && manager.current();
-
-    if (view && view.expand) {
-
-        view.expand();
-
-    }
+    
 
 }
 document.getElementById("next").onclick = () => turnPage("next");
