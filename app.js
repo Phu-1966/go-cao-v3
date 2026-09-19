c943361d48a8b9fc4e4440a6dccbccb6a39060c9
Phu-1966 <phupham2025@gmail.com>
2026-09-19 10:21:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ let fontSize = 100;
 function renderToc(items, parent) {
   items.forEach(item => {
     const a = document.createElement("a");
+    a.style.marginLeft = "12px";
     a.textContent = item.label;
     a.href = "#";
     a.onclick = e => { e.preventDefault(); rendition.display(item.href); };
