01cc27516b4bccfc622d66ac0304bab9373b01e8
Phu-1966 <phupham2025@gmail.com>
2026-09-20 14:47:29 +0700

    Fix path to EPUB file in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const book = ePub("SU_ICH_KY_THONG_MINH_FINAL_V2.epub", {});
+const book = ePub(./"SU_ICH_KY_THONG_MINH_FINAL_V2.epub", {});
 const rendition = book.renderTo("viewer", {
 
   width: document.getElementById("viewer").clientWidth,
