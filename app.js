3c48a5b703f2c9211dceb23f7e90bf6c2628d5f3
Phu-1966 <phupham2025@gmail.com>
2026-09-22 19:43:58 +0700

    Remove linkHook deregistration logic
    Remove unused linkHook deregistration logic from app.js.

--- app.js
+++ app.js
@@ -17,17 +17,7 @@ gap: 0 ,
 });
 
 rendition.spread("none");
-const linkHook = rendition.hooks.content.hooks.find(
-
-  fn => fn.name === "bound handleLinks"
-
-);
-
-if (linkHook) {
-
-  rendition.hooks.content.deregister(linkHook);
-
-}
+rendition.hooks.content.hooks.shift();
 rendition.on("linkClicked", href => {
 
   alert("EPUB link:\n" + href);
