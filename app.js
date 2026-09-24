ba886fd291203def2a85c37e17c746477c967633
Phu-1966 <phupham2025@gmail.com>
2026-09-24 22:42:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -82,9 +82,15 @@ function renderToc(items, parent) {
     const a = document.createElement("a");
     a.style.marginLeft = "20px";
     a.textContent = item.label;
-    a.href = item.href;
+    a.href = "#";
 
-    parent.appendChild(a);
+a.onclick = function(e) {
+
+  e.preventDefault();
+
+  rendition.display(item.href);
+
+};
     if (item.subitems && item.subitems.length) renderToc(item.subitems, parent);
   });
 }
