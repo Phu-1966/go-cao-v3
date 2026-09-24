6fa2d241672cb1bb4abeca62ee743a1d3ec72218
Phu-1966 <phupham2025@gmail.com>
2026-09-24 19:49:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,13 @@ doc.querySelectorAll("a").forEach(a => {
 });
 
 }
+contents.on("link", href => {
 
+  const relative = book.path.relative(href);
+
+  rendition.display(relative);
+
+});
 });
 
 let fontSize = 100;
